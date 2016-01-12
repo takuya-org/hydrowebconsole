2da3186cc7589c3b1c19d1de4538e11e7de326d4
tanpopo <b2094@oic.jp>
2016-01-12 12:49:22 +0900

    レコード取得用クラス作成

---
+++ dbaccess.py
@@ -0,0 +1,23 @@
+import json
+import sqlite3
+class db_operation(object):
+    DIR = 'dbtest/'
+    FILE_NAME = 'hydroponics.db'
+
+    def __init__(self):
+        self.con = sqlite3.Connection(db_operation.DIR + db_operation.FILE_NAME)
+        c = self.con.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS temp(date text, time text, temp_c real, PRIMARY KEY(date, time))""")
+
+    def selectRecord(self, limit, offset):
+        c = self.con.cursor()
+        c.execute("SELECT * FROM temp LIMIT ? OFFSET ?", (limit, offset))
+        date_list = []
+        temp_list = []
+        for date, temp in c.fetchall():
+            date_list.append(date)
+            temp_list.append(temp)
+        return json.dumps({'temperature':temp_list, 'period':date_list})
+
+    def connectionClose():
+        self.con.close()
