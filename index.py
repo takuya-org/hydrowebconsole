fc1cd7dc9f4be0968606d95c2041879032aaae7e
oniipon <b2142@oic.jp>
2016-01-19 17:46:03 +0900

    小姑に怒られたので，の後にスペースを入れたでゲソ

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from bottle import static_file
 import dbaccess
 import subprocess
 
+
 @route('/')
 def index():
     dbo = dbaccess.DbOperation()
@@ -10,13 +11,16 @@ def index():
     dbo.connectionClose()
     return template('index.html', temp_list=temp_list)
 
+
 @route('/node/<filepath:path>')
 def node_static(filepath):
     return static_file(filepath, root='./node_modules')
 
+
 @route('/css/<filepath:path>')
 def css_static(filepath):
     return static_file(filepath, root='./css')
+
 
 @route('/js/<filepath:path>')
 def js_static(filepath):
@@ -25,5 +29,5 @@ def js_static(filepath):
 
 cmd = 'hostname'
 p = subprocess.check_output(cmd)
-name = str(p,encoding='utf-8')
+name = str(p, encoding='utf-8')
 run(host=name.rstrip(), port=8080, debug=True)
